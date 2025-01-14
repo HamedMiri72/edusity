b6fcc2416834e7ead3411547487e943dd17ed1b6
HamedMiri72 <hamedmiri72@gmail.com>
2025-01-14 18:22:07 +0000

    gallary campus

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,13 @@ const Navbar = () => {
 
   const [sticky, setSticky] = useState(false);
 
+
+  useEffect(()=>{
+    window.addEventListener("scroll", ()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    })
+  }, [])
+
   useEffect(() => {
     window.addEventListener("scroll", ()=>{
       window.scrollY > 50 ? setSticky(true) : setSticky(false)
