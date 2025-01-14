ea75d472819bd392f919f86d37ac0a68c38ec830
HamedMiri72 <hamedmiri72@gmail.com>
2025-01-14 17:40:20 +0000

    programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Navbar/Hero/Hero'
+import Programs from './components/programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className='container'>
+      <Programs/>
+      </div>
+      
     </div>
   )
 }
